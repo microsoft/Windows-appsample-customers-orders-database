deaca50746e9f9e63d028c4452eddb23808f7fdd
Alexander Koren <alkoren@microsoft.com>
2017-03-06 11:06:36 -0800

    UI updates

--- ContosoApp/UserControls/DetailExpander.xaml.cs
+++ ContosoApp/UserControls/DetailExpander.xaml.cs
@@ -1,18 +1,18 @@
 //  ---------------------------------------------------------------------------------
 //  Copyright (c) Microsoft Corporation.  All rights reserved.
-// 
+//
 //  The MIT License (MIT)
-// 
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions:
-// 
+//
 //  The above copyright notice and this permission notice shall be included in
 //  all copies or substantial portions of the Software.
-// 
+//
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -87,11 +87,16 @@ namespace ContosoApp.UserControls
         public static readonly DependencyProperty LabelStyleProperty =
             DependencyProperty.Register("LabelStyle", typeof(Style), typeof(DetailExpander), new PropertyMetadata(null));
 
-        private void ToggleButton_Checked(object sender, RoutedEventArgs e) => 
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e) =>
             (sender as ToggleButton).Style = Resources["ExpanderButtonOpenStyle"] as Style;
 
-        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e) => 
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e) =>
             (sender as ToggleButton).Style = Resources["ExpanderButtonClosedStyle"] as Style;
+
+        private void Header_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            Toggle.IsChecked = !Toggle.IsChecked;
+        }
     }
 
 
@@ -110,6 +115,6 @@ namespace ContosoApp.UserControls
         /// Converts bool to nullable bool.
         /// </summary>
         public object ConvertBack(object value, Type targetType, object parameter, string language) =>
-            (bool)value; 
+            (bool)value;
     }
 }
